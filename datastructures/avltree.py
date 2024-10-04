f0cc38e8ba95efb24878fa237d80de3954d2c65c
mvloper <milenavloper@gmail.com>
2024-10-04 11:46:20 -0700

    setting up AVLTree

--- datastructures/avltree.py
+++ datastructures/avltree.py
@@ -1,17 +1,29 @@
 from __future__ import annotations
-from iavltree import IAVLTree, K, V
+from datastructures.iavltree import IAVLTree, K, V
 from typing import Generic, Callable, List, Optional, Sequence, Tuple
 
 
 
-@dataclass
+# @dataclass
 class AVLNode(Generic[K, V]):
-   def __init__(self, key: K, value: V, left: Optional[AVLNode]=None, right: Optional[AVLNode]=None):
+    def __init__(self, key: K, value: V, left: Optional[AVLNode]=None, right: Optional[AVLNode]=None):
       self.key = key
       self.value = value
       self.left = left
       self.right = right
       self.height = 1
+
+    def balance(self) -> int:
+        L = 0 if self.left == None else self.left.height
+        R = 0 if self.right == None else self.right.height
+        return L - R
+
+    def update_height(self) -> int:
+        self.height = 1 + max(
+            0 if self.left == None else self.left.height,
+            0 if self.right == None else self.right.height
+        )
+        return self.height
 
 # height = max(left.height, right.height) + 1
 # balance = left.height - right.height
@@ -20,6 +32,7 @@ class AVLNode(Generic[K, V]):
 class AVLTree(IAVLTree[K, V], Generic[K,V]):
 
     def __init__(self, starting_sequence: Optional[Sequence[Tuple]]=None):
+        self.root = None
         if starting_sequence is not None:
             for key, value in starting_sequence:
                 self.insert(key, value)
@@ -31,8 +44,56 @@ class AVLTree(IAVLTree[K, V], Generic[K,V]):
                 key (K): The key used to order the nodes in the tree.
                 value (V): The value associated with the key.
         """
-        if tree is empty create new node
-        pass
+        self.root = self.insert_helper(key=key, value=value, node=self.root)
+        return
+
+    def insert_helper(self, key: K, value: V, node: AVLNode) -> AVLNode:
+        if node is None:
+            return AVLNode(key=key, value=value)
+        elif key < node.key:
+            node.left = self.insert_helper(key=key, value=value, node=node.left)
+        else:
+            node.right = self.insert_helper(key=key, value=value, node=node.right)
+
+        node.update_height()
+
+        return self.balance_tree(node=node)
+
+    def balance_tree(self, node: AVLNode) -> AVLNode:
+        if node.balance() > 1:
+            if node.left.balance() < 0:
+                node.left = self.rotate_left(node.left)
+            node = self.rotate_right(node)
+
+        elif node.balance() < -1:
+            if node.right.balance() > 0:
+                node.right = self.rotate_right(node.right)
+            node = self.rotate_left(node)
+
+        return node
+
+    def rotate_left(self, node: AVLNode) -> AVLNode:
+        new_root = node.right
+        new_left_subtree = new_root.left
+        node.right = new_left_subtree
+        new_root.left = node
+
+        node.update_height()
+        new_root.update_height()
+
+        return new_root
+
+    def rotate_right(self, node:AVLNode) -> AVLNode:
+        new_root = node.left
+        new_right_subtree = new_root.right
+        node.left = new_right_subtree
+        new_root.right = node
+
+        node.update_height()
+        new_root.update_height()
+
+        return new_root
+        
     
     def search(self, key: K) -> Optional[V]:
         """Searches for a key in the binary search tree and returns the associated value if found.
@@ -65,7 +126,20 @@ class AVLTree(IAVLTree[K, V], Generic[K,V]):
         Returns:
             List[K]: The list of keys in inorder traversal.
         """
-        pass
+        return self.inorder_helper(self.root)
+
+    def inorder_helper(self, node:AVLNode) -> List[K]:
+        subtree = []
+        if node.left != None:
+            subtree.extend(
+                self.inorder_helper(node.left)
+            )
+        subtree.append(node.key)
+        if node.right != None:
+            subtree.extend(
+                self.inorder_helper(node.right)
+            )
+        return subtree
 
     def preorder(self, visit: Optional[Callable[[V], None]]=None) -> List[K]:
         """Returns the preorder traversal of the binary search tree.
@@ -95,7 +169,13 @@ class AVLTree(IAVLTree[K, V], Generic[K,V]):
         Returns:
             List[K]: The list of keys in breadth-first order.
         """
-        pass
+        return self.bf_helper(self.root)
+    
+    def bf_helper(self, node: AVLNode) -> List[K]:
+        subtree = [ node.key ]
+        subtree.extend(self.bf_helper(node.left))
+        subtree.extend(self.bf_helper(node.right))
+        return subtree
 
     def size(self) -> int:
         """Returns the number of nodes in the binary search tree.
--- program.py
+++ program.py
@@ -1,10 +1,40 @@
-from tests.car import Car, Color, Make, Model
+from datastructures.avltree import AVLTree, AVLNode
+import random
 
-def main():
-    print('Hello world!')
+# sequence0 = [(0,0), (1,1), (2,2), (3,3), (4,4), (5,5), (6,6)]
+# sequence = []
 
-    car = Car(vin='123456789', color=Color.RED, make=Make.TOYOTA, model=Model.COROLLA)
-    print(car)
+# for i in range(len(sequence0)):
+#     x = random.choice(sequence0)
+#     sequence.append(x)
+#     sequence0.remove(x)
 
-if __name__ == '__main__':
-    main()
+# print(sequence)
+
+# tree = AVLTree(starting_sequence=sequence)
+
+# print(f"     {tree.root.key}")
+# print(f"   /   \\")
+# print(f"  {tree.root.left.key}     {tree.root.right.key}")
+# print(f" / \\   / \\")
+# print(f"{tree.root.left.left.key}   {tree.root.left.right.key} {tree.root.right.left.key}   {tree.root.right.right.key}")
+
+
+
+
+
+
+
+
+
+
+# from tests.car import Car, Color, Make, Model
+
+# def main():
+#     print('Hello world!')
+
+#     car = Car(vin='123456789', color=Color.RED, make=Make.TOYOTA, model=Model.COROLLA)
+#     print(car)
+
+# if __name__ == '__main__':
+#     main()
