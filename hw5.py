d2909a9b35e73f944cd99763d9105db95fe1e85c
Milena <121083052+mvloper@users.noreply.github.com>
2024-10-23 16:59:31 -0700

    Add files via upload

--- hw5.py
+++ hw5.py
@@ -1,6 +1,21 @@
-from datastructures.intervaltree import IntervalNode, IntervalTree
+# from datastructures.intervaltree import IntervalNode, IntervalTree
 
-test = IntervalNode(low=1)
+# test = IntervalNode(low=1)
 
-print(test.height)
-print(test.intervals_at_low)
+# print(test.height)
+# print(test.intervals_at_low)
+
+from datastructures.intervaltree import IntervalTree
+
+tree = IntervalTree()
+
+tree.interval_insert(high=5, low=2, data=[5, 2, "2-5", ":)"])
+tree.interval_insert(high=8, low=2, data=[8, 2, "2-8", ":D"])
+tree.interval_insert(high=9, low=5, data=[9, 5, "5-9", ":P"])
+tree.interval_insert(high=2, low=1, data=[2, 1, "1-2", ":O"])
+tree.interval_insert(high=10, low=1, data=[10, 1, "1-10", ":|"])
+
+print(tree.search(point=3)) # should have 5, 8, 10
+print(tree.search(point=8)) # should have 8, 9, 10
+print(tree.search(point=9)) # should have 9, 10
+print(tree.inorder())
